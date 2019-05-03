510679323dbd4ac7211c399e7fe81bfd767f330b
Julian Santana <jjsantan@my.loyno.edu>
2019-05-03 00:49:47 -0400

    add sample env and comments

--- app.rb
+++ app.rb
@@ -12,6 +12,9 @@ require './config/environments'
 class App < Sinatra::Base
   enable :sessions
 
+## Schedules job to subtract a
+## day from every active picture
+## and deactivates image if days reaches 0
   scheduler = Rufus::Scheduler.new
   scheduler.every '1d' do
     photos = Photo.active
@@ -24,9 +27,11 @@ class App < Sinatra::Base
 
   helpers do
     def loged
+      ## When attempted to go to any page
+      ## while not logged in redirects to home
       redirect '/' if session[:id].nil?
     end
-
+      ## Requires https
     def https_required!
       if settings.production? && request.scheme == 'http'
         headers['Location'] = request.url.sub('http', 'https')
@@ -38,14 +43,18 @@ class App < Sinatra::Base
   before do
     https_required!
   end
-
+    #login page
   get '/' do
     erb :login
   end
 
+    ## App has one hard coded user
+    ## per client request
   post '/' do
     if params[:user] == ENV['USERNAME'] && params[:pass] == ENV['PASS']
+    ## stores session to remember user is logged in
       session[:id] = params[:user]
+      ## Rediricts to create if successful
       redirect '/crear'
     else
       "contrasena incorecta porfavor intente denuevo <a href='/'>Inicio</a>"
@@ -54,11 +63,13 @@ class App < Sinatra::Base
   get '/index' do
     erb :index
   end
-
+    ## This page shows displays and pictures
+    ## assigned to screen
+    ## accepts param display to show specific screen
   get '/envivo' do
     loged
     @display = params[:display].to_i unless params[:display].nil?
-
+    ##defaults to screen 1
     @photos = if @display.nil?
                 Photo.where(display: 1)
               elsif @display == 7
@@ -70,8 +81,11 @@ class App < Sinatra::Base
   end
 
   post '/envivo' do
+    ## Updates screen images.
     loged
-
+    ## if params[:display] is 7 then it
+    ## does action on all screens for
+    ## specific image
     if params[:display].to_i == 7
       unless params[:activate].nil?
         params[:activate].each do |i|
@@ -82,6 +96,7 @@ class App < Sinatra::Base
           end
         end
     end
+    ## Deactivates images that still have days left
       unless params[:deactivate].nil?
         params[:deactivate].each do |i|
           temp = Photo.find_by(id: i.to_i)
@@ -96,7 +111,9 @@ class App < Sinatra::Base
         params[:delete].each do |i|
           temp = Photo.find_by(id: i.to_i)
           temp_delete = Photo.where(public_url: temp.public_url)
+          ## Get aws client from env credentials
           client = get_aws_client
+          ## Deletes images from aws bucket
           temp_delete.each do |y|
             client.delete_object( bucket: "centralphoto", key: y.photo_name.to_s)
           end
@@ -104,6 +121,9 @@ class App < Sinatra::Base
         end
     end
     else
+      ## does action on image
+      ## for specific screen
+      ## Activates images for an extra day
       unless params[:activate].nil?
         params[:activate].each do |i|
           temp = Photo.find_by(id: i.to_i)
@@ -123,9 +143,10 @@ class App < Sinatra::Base
         end
     end
 end
+## once done redirect to edit page
     redirect '/envivo'
   end
-
+## page to add images
   get '/crear' do
     loged
     erb :entrar
@@ -147,6 +168,9 @@ end
       upload_all = nil
       upload_file = f[:tempfile]
       days = params[:days].to_i
+      ## if todas is true then upload images
+      ## to all screens
+      ## else set display to specific screen
       if params[:todas].nil?
         display = params[:display].to_i
       else
