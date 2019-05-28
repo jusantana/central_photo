26b66a8f0d43849e21b2719a5956e58022981691
Julian Santana <jjsantan@my.loyno.edu>
2019-05-28 18:29:14 -0400

    testing env

---
+++ spec/features/routes_spec.rb
@@ -0,0 +1,20 @@
+
+require_relative '../spec_helper'
+
+
+RSpec.describe 'Sinatra App' do
+
+  it 'has a homepage' do
+    get '/'
+    expect(last_response).to be_ok
+  end
+  it 'logins' do
+    post '/', {'user' => ENV['USERNAME'],'pass' => ENV['PASS']}
+    byebug
+    expect(last_response).to be_ok
+  end
+  it 'has crear' do
+    get '/crear'
+    expect(last_response).to be_ok
+  end
+end
